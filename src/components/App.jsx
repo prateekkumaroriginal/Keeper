a19cb5431e4ccd540c2e93720edc9e01865fdbb4
Prateek <prateek.kumar.original@gmail.com>
2023-08-06 18:54:45 +0530

    Add new note

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,15 +1,23 @@
-import React from "react";
+import React, { useState } from "react";
 import Header from "./Header"
 import Footer from "./Footer"
 import Note from "./Note"
-import notes from "../notes";
+import AddNewNote from "./AddNewNote";
 
 export default function App() {
+    const [notes, setNotes] = useState([])
+    const addNote = (note) => {
+        setNotes(prev=>{
+            return [...prev, note]
+        })
+    }
+
     return (<>
         <Header />
-        {notes.map((note) => (
+        <AddNewNote onAdd={addNote} />
+        {notes.map((note, i) => (
             <Note
-                key={note.key}
+                key={i}
                 title={note.title}
                 content={note.content}
             />
